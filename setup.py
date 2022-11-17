f9e7db154b05450197e8b400647f4f18d97f07d3
SGProduction <100243269+SGProduction@users.noreply.github.com>
2022-11-17 15:42:48 +1100

    Can actually write to new gatt_sensor file
    - unlike prev commit

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@
 
 # For distribution
 
-'''from distutils.core import setup
-import py2exe
-  
-setup(console=['serial_injector.py'])'''
+import os
 
+os.system("echo:")
+os.system("py --version")
+os.system("echo Running builder to package python script into a single executable:\n")
+os.system("echo:")
+
+os.system("pyinstaller serial_injector.py --onefile")
