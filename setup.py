53274c3dcaafc1e5655df2eaaeb07307ae570bf8
josh-kruik <112363113+josh-kruik@users.noreply.github.com>
2023-02-09 12:21:45 +1100

    Made .bat file to auto build new serial injector.exe
    - changed folder location

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ import os
 import PyInstaller.__main__
 
 os.system("echo:")
-os.system("py --version")
+os.system("python --version")
 os.system("echo Running builder to package python script into a single executable:\n")
 os.system("echo:")
 
 #os.system("pyinstaller serial_injector.py --onefile")
 
-
 PyInstaller.__main__.run([
-    'serial_injector.py',
+    'serial_injector.pyw',
     '--onefile',
     '--icon=pouring-breakfast-cereal-B9YDP3.ico'
    # '--windowed' #for building a mac app
